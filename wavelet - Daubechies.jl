53f5e2a1246405cfeee29fa0e141747bd5fd0ef9
nyasyamorina <44353997+nyasyamorina@users.noreply.github.com>
2022-01-19 16:14:56 +0800

    Add files via upload

---
+++ wavelet - Daubechies.jl
@@ -0,0 +1,30 @@
+include("wavlet - funcs.jl")
+using Polynomials, Plots
+
+
+function genDaubechiesCoefs(N::Integer)
+    (n < 1) && throw(ArgumentError("N-order Daubechies wavelet needs `N` > 0"))
+    poly = Polynomial([1, 1])^N
+    for q ∈ roots(Polynomial([binomial(N - 1 + k, k) for k ∈ 0:N - 1]))
+        r = 1 - 2 * (q + √(q * (q - 1)))
+        (abs2(r) ≥ 1) && (r = 1 / r)        # ?
+        poly *= Polynomial([-r, 1])
+    end
+    c = reverse(real.(poly.coeffs))
+    return CoefP((c .* (2 / sum(c)))...)
+end
+
+
+# doesn't need reversing version
+
+function genDaubechiesCoefs_(N::Integer)
+    (n < 1) && throw(ArgumentError("N-order Daubechies wavelet needs `N` > 0"))
+    poly = Polynomial([1, 1])^N
+    for q ∈ roots(Polynomial([binomial(N - 1 + k, k) for k ∈ 0:N - 1]))
+        r = 1 - 2 * (q - √(q * (q - 1)))
+        (abs2(r) ≤ 1) && (r = 1 / r)        # ?
+        poly *= Polynomial([-r, 1])
+    end
+    c = real.(poly.coeffs)
+    return CoefP((c .* (2 / sum(c)))...)
+end
