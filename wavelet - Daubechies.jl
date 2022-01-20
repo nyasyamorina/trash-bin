474ba7d50bd26e6f2451f8bf04e1ec0bd4ce9a98
nyasyamorina <44353997+nyasyamorina@users.noreply.github.com>
2022-01-20 14:19:42 +0800

    Update wavelet - Daubechies.jl

--- wavelet - Daubechies.jl
+++ wavelet - Daubechies.jl
@@ -3,7 +3,7 @@ using Polynomials
 
 
 function genDaubechiesCoefs(N::Integer)
-    (n < 1) && throw(ArgumentError("N-order Daubechies wavelet needs `N` > 0"))
+    (N < 1) && throw(ArgumentError("N-order Daubechies wavelet needs `N` > 0"))
     poly = Polynomial([1, 1])^N
     for q ∈ roots(Polynomial([binomial(N - 1 + k, k) for k ∈ 0:N - 1]))
         r = 1 - 2 * (q + √(q * (q - 1)))
@@ -18,7 +18,7 @@ end
 # doesn't need reversing version
 
 function genDaubechiesCoefs_(N::Integer)
-    (n < 1) && throw(ArgumentError("N-order Daubechies wavelet needs `N` > 0"))
+    (N < 1) && throw(ArgumentError("N-order Daubechies wavelet needs `N` > 0"))
     poly = Polynomial([1, 1])^N
     for q ∈ roots(Polynomial([binomial(N - 1 + k, k) for k ∈ 0:N - 1]))
         r = 1 - 2 * (q - √(q * (q - 1)))
